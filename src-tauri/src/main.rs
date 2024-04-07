1b54ebb873b9691d955057b77d008f68377e250b
crdjm <djm@logictran.com>
2024-04-07 15:49:04 -0500

    Save epub, and initial read of metadata

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,24 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::collections::HashMap;
 use std::fs;
+use std::io::Read;
 
 use std::path::Path;
 use std::path::PathBuf;
-// use tauri::api::file;
 use tauri::api::path::cache_dir;
-// use std::io::{stdout, Write};
-// use std::fs::File;
 use zip_extensions::*;
 
 use std::fs::File;
-use std::io::{self, Write};
+use std::io::Write;
 use zip::write::{FileOptions, ZipWriter};
 use zip::CompressionMethod;
 
+use epub::doc::EpubDoc;
+use walkdir::WalkDir;
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, expand, create_epub])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            expand,
+            create_epub,
+            get_metadata
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -29,40 +36,53 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
+fn get_metadata(path: &str, name: &str, fullpath: &str) -> HashMap<String, Vec<String>> {
+    let doc = EpubDoc::new(fullpath).unwrap();
+    // let title = doc.mdata("title").unwrap();
+
+    doc.metadata
+    // format!("epub path: {}, title = {}", fullpath, title)
+}
+
+#[tauri::command]
 fn expand(name: &str) -> String {
     println!("The name param is {}", name);
     let a = false;
+    let mut result = String::new();
 
     // I clearly don't understand RUST file handling, specifically the need to keep cloning things
     // Will reseatch and update in the future
-    if a {
-        let name_path = PathBuf::from(name);
+    let name_path = PathBuf::from(name);
 
-        match cache_dir() {
-            Some(path) => {
-                let base = name_path.file_name().expect("no basename");
-                let stem = name_path.file_stem().expect("no file stem");
-                let mut dest = PathBuf::from(path);
-                dest.push("epubai");
-                dest.push(stem);
-                let folder = dest.clone();
-                fs::create_dir_all(dest.clone()).expect("Failed to create");
+    match cache_dir() {
+        Some(path) => {
+            let base = name_path.file_name().expect("no basename");
+            let stem = name_path.file_stem().expect("no file stem");
+            let mut dest = PathBuf::from(path);
+            dest.push("epubai");
+            dest.push(stem);
+            let folder = dest.clone();
+            fs::create_dir_all(dest.clone()).expect("Failed to create");
 
-                dest.push(base);
+            dest.push(base);
 
-                let _result = fs::copy(name, dest.clone());
+            let _result = fs::copy(name, dest.clone());
 
-                let archive_file: PathBuf = dest.clone();
-                let target_dir: PathBuf = folder;
+            let archive_file: PathBuf = dest.clone();
+            let target_dir: PathBuf = folder;
+            result = target_dir.to_str().unwrap().to_string();
+            println!("The result is {:?} basename {:?} file ", result, base);
+            if a {
                 zip_extract(&archive_file, &target_dir).expect("Failed to extract");
-
-                // println!("The result is {:?} basename {:?} file ", result, base);
             }
-            None => {
-                println!("The cache dir is None");
-            }
+            // println!("The result is {:?} basename {:?} file ", result, base);
+        }
+        None => {
+            println!("The cache dir is None");
         }
     }
+
+    result
 
     // To ZIP
     // let archive_file: PathBuf = ...
@@ -72,29 +92,30 @@ fn expand(name: &str) -> String {
 
     // println!("The result is {:?}", result);
     // println!("The cache dir is {}", cache_dir().expect('TEST').to_str());
-    format!("Expand, {}!", name)
+    // format!("Expand, {}!", name)
 }
-
-// use std::fs::{self, File};
-// use std::io::{self, Write};
-// use std::path::{Path, PathBuf};
-// use zip::write::{FileOptions, ZipWriter};
-// use zip::CompressionMethod;
 
 #[tauri::command]
 fn create_epub(name: &str, output: &str) -> String {
     println!("Creating EPUB from folder {} to {}", name, output);
 
+    let walkdir = WalkDir::new(name);
+    let it = walkdir.into_iter();
+
+    // Create a new EPUB file
+    // let epub_file1 = File::create(output).expect("Failed to create EPUB file");
+    let epub_file = File::create(output).expect("Failed to create EPUB file");
+
+    // let _ = zip_dir(&mut it.filter_map(|e| e.ok()), name, epub_file1);
+
     // name is the original epub name, we need to map that to the cache folder containing the expanded files...
     // OR pass that back to the UI and back to here
 
-    // Create a new EPUB file
-    let epub_file = File::create(output).expect("Failed to create EPUB file");
     let mut zip_writer = ZipWriter::new(epub_file);
 
     // Add mimetype file (not compressed)
-    let mut options = FileOptions::default();
-    options.compression_method(CompressionMethod::Stored);
+    let options = FileOptions::default();
+    let _ = options.compression_method(CompressionMethod::Stored);
     zip_writer
         .start_file("mimetype", options)
         .expect("mimetype failed");
@@ -103,46 +124,56 @@ fn create_epub(name: &str, output: &str) -> String {
         .expect("mimetype write failed");
 
     // Set compression method to deflate for other files
-    let mut optionsDeflate = FileOptions::default();
-    options.compression_method(CompressionMethod::Deflated);
+    let options_deflate = FileOptions::default();
+    let _ = options_deflate.compression_method(CompressionMethod::Deflated);
     // zip_writer.set_options(FileOptions::default().compression_method(CompressionMethod::Deflated));
 
     // Iterate over files in the folder
     let folder = Path::new(name);
-    for entry in fs::read_dir(folder).expect("Failed to read directory") {
+    let mut buffer = Vec::new();
+    for entry in it {
+        // for entry in fs::read_dir(folder).expect("Failed to read directory") {
         let entry = entry.expect("Failed to read entry");
         let file_path = entry.path();
 
-        println!("The file path is {}", file_path.to_str().unwrap());
+        // println!("The file path is {}", file_path.to_str().unwrap());
 
-        //         // Get relative path for zipping
-        //         let relative_path = file_path.strip_prefix(folder).unwrap();
+        // Get relative path for zipping
+        let name = file_path.strip_prefix(folder).unwrap();
 
-        //         // Skip mimetype file
-        //         if relative_path.to_str().unwrap() == "mimetype" {
-        //             continue;
-        //         }
+        // Skip mimetype file
+        if name.to_str().unwrap() == "mimetype" {
+            continue;
+        }
 
-        //         // Create a zip file entry and add it to the EPUB
-        //         zip_writer.start_file(relative_path.to_str().unwrap(), FileOptions::default())?;
-        //         let mut file = File::open(&file_path)?;
-        //         io::copy(&mut file, &mut zip_writer)?;
+        if name.to_str().unwrap().to_lowercase().contains(".epub") {
+            continue;
+        }
+
+        if file_path.is_file() {
+            // Create a zip file entry and add it to the EPUB
+            // zip_writer.start_file(relative_path.to_str().unwrap(), FileOptions::default())?;
+            let _ = zip_writer.start_file(name.to_str().unwrap(), options_deflate);
+            let mut file = File::open(&file_path).expect("Failed to open file");
+
+            file.read_to_end(&mut buffer).expect("Failed to read file");
+            zip_writer.write_all(&buffer).expect("Failed to write file");
+            buffer.clear();
+        } else if !name.as_os_str().is_empty() {
+            // println!("adding dir {file_path:?} as {name:?} ...");
+            #[allow(deprecated)]
+            let _ = zip_writer
+                .add_directory_from_path(name, options_deflate)
+                .expect("Failed to add directory");
+        }
     }
 
     //     // Finish writing the EPUB file
-    //     zip_writer.finish()?;
+    zip_writer
+        .finish()
+        .expect("Failed to finish writing EPUB file");
 
     //     println!("EPUB file '{}' created successfully!", output_filename);
 
     format!("Expand, {}!", name)
 }
-
-// fn main() {
-//     let folder_path = "path/to/your/folder";
-//     let output_filename = "output.epub";
-
-//     match create_epub_from_folder(folder_path, output_filename) {
-//         Ok(_) => println!("EPUB creation successful!"),
-//         Err(e) => eprintln!("Error: {}", e),
-//     }
-// }
--- src/app/imageList.tsx
+++ src/app/imageList.tsx
@@ -1,7 +1,11 @@
+import { useEffect, useState } from 'react';
+
 import { Button } from '@/components/ui/button';
 // import { Input } from '@/components/ui/input';
 import { invoke } from '@tauri-apps/api/tauri';
-import { useEffect, useState } from 'react';
+import { save } from '@tauri-apps/api/dialog';
+import path from 'path';
+
 
 interface Props {
     epubName: string;
@@ -11,35 +15,64 @@ export default function ImgageList(props: Props) {
     const epubName = props.epubName;
 
     // const [epub, setEpub] = useState<any>(null);
-    const [greeting, setGreeting] = useState('');
+    const [epubPath, setEpubPath] = useState('');
+    const [message, setMessage] = useState('');
+    const [saveAs, setSaveAs] = useState<String | null>(null);
 
     useEffect(() => {
 
         invoke<string>('expand', { name: epubName })
-            .then(result => setGreeting(result))
+            .then(result => setEpubPath(result))
             .catch(console.error);
 
 
 
     }, [])
 
-    async function create_epub() {
-        const output = epubName.replace(".epub", "_output.epub");
-        // alert("Creating epub " + epubName);
-        try {
-            let res = await invoke<string>('create_epub', { name: epubName, output: output });
-            alert(res);
-            // .then(result => setGreeting(result))
-            // .catch(console.error);
-        } catch (err) { alert(err) }
+
+    async function save_epub() {
+        const path = await save({
+            filters: [
+                {
+                    name: "Save Epub",
+                    extensions: ['epub'],
+                },
+            ],
+            defaultPath: epubName.replace(".epub", "_output.epub"),
+            title: "Save Epub",
+        });
+        if (path) {
+            setSaveAs(path);
+
+            const output = (saveAs ? saveAs : epubName.replace(".epub", "_output.epub"));
+            try {
+                let res = await invoke<string>('create_epub', { name: epubPath, output: output });
+                setMessage(res);
+            } catch (err) { alert(err) }
+        }
     }
 
 
+    async function get_metadata() {
+        const base = path.basename(epubName);
+        try {
+            let res: any = await invoke<string>('get_metadata', { path: epubPath, name: base, fullpath: path.join(epubPath, base) });
+
+            // setMessage(JSON.stringify(res, null, 2) + "\n");
+            setMessage(res.title);
+        } catch (err) { alert(err) }
+
+    }
+
     return <div className="flex flex-col space-y-5 sm:px-12 bg-slate-100 h-full min-h-screen">
-        <div className="flex w-full justify-center p-8">
+        <div className="flex flex-col w-full justify-center p-8">
             <h1>{epubName}</h1>
-            <h2>{greeting}</h2>
-            <Button onClick={create_epub}>Create epub</Button>
+            <h2>{epubPath}</h2>
+            <h2>{saveAs}</h2>
+            <h3>{message}</h3>
+            <Button onClick={save_epub}>Save epub</Button>
+
+            <Button onClick={get_metadata}>Get Metadata</Button>
         </div>
 
     </div >
